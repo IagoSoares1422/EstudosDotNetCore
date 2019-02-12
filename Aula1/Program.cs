e5e7080a9124aa8642b39cee5c192800b5b4ad72
Iago <iago_soares@estudante.sc.senai.br>
2019-02-12 11:34:04 -0200

    Criando projeto DotNetCore

---
+++ Aula1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+            .UseKestrel()
+                .Configure(app => {
+                    app.Run(context => context.Response.WriteAsync("Seja Bem vindo!!"));
+                })
+                .Build();
+            host.Run();
+        }
+    }
+}
